ec5ea031307f5fdb79babae47073a14029ca1162
Atul Kumar <atulkumarrpvvyv0740@gmail.com>
2021-11-04 08:10:49 +0530

    04/11-08:10

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,24 @@ import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
 
 const Form = () => {
-    
+
     const [email, setemail] = useState("")
-    const [emailError, setemailError] = useState('')
-    const [passwordError, setpasswordError] = useState('')
+    const [emailError, setemailError] = useState('.')
+    const [passwordError, setpasswordError] = useState('.')
     const [password, setpassword] = useState("")
     const handleemails = (e) => {
         setemail(e.target.value);
-        setemailError('');
+        setemailError('.');
+        document.getElementById('email-errp').style.visibility='hidden';
     };
     const handlepasswords = (e) => {
         setpassword(e.target.value);
-        setpasswordError('');
+        setpasswordError('.');
+        document.getElementById('pass-errp').style.visibility='hidden';
     };
     useEffect(() => {
         let label = document.getElementById('email-labelled');
-        let input=document.getElementById('input-email');
+        let input = document.getElementById('input-email');
         if (email === "") {
             label.classList.remove('email-label-focussed');
             input.classList.remove('outfocus');
@@ -31,10 +33,10 @@ const Form = () => {
             input.classList.remove('infocus')
         }
     }, [email])
-    
+
     useEffect(() => {
         let label = document.getElementById('password-labelled');
-        let input=document.getElementById('input-password');
+        let input = document.getElementById('input-password');
         if (password === "") {
             label.classList.remove('password-label-focussed');
             input.classList.remove('outfocus');
@@ -46,48 +48,58 @@ const Form = () => {
             input.classList.remove('infocus')
         }
     }, [password])
-    const valid = () =>{
-        if(email==='' || password===''){
-            if(email==='')
-            setemailError('Please Enter Email')
-            if(password==='')
-            setpasswordError('Please Enter Password')
+    const valid = () => {
+        if (email === '' || password === '') {
+            if (email === ''){
+                setemailError('Please Enter Email')
+                document.getElementById('email-errp').style.visibility='visible';
+            }
+            if (password === ''){
+                setpasswordError('Please Enter Password')
+                document.getElementById('pass-errp').style.visibility='visible';
+            }
             return false;
         }
-        if(email.includes('@') && email.includes('.'));
-        else{
+        if (email.includes('@') && email.includes('.'));
+        else {
             setemailError('Enter Valid Email')
+            document.getElementById('email-errp').style.visibility='visible';
             return false;
-        } 
+        }
         return true;
     }
-    const HandleSubmit=(e)=>{
+    const HandleSubmit = (e) => {
         e.preventDefault();
-        if(valid()){
-            setemail('')
+        if (valid()) {
+            setemail('');
             setpassword('')
         }
     }
     return (
-        <div className='form' data-aos="zoom-in">
+        <div className='form' >
             <div className="text">
                 <div className="logo">
                     <a href="https://www.thesocialcomment.com/" target="_blank" rel="noreferrer"> thesocialcomment</a>
                 </div>
                 <div className="page-text">Welcome Back. Please Login</div>
                 <form onSubmit={HandleSubmit} className='login'>
-                    <div className='input-content infocus' id='input-email'>
-                        <i class="fa fa-envelope"></i>
-                        <input type="email" name="email" className='email-input' autoComplete="off" id="email" required value={email} onChange={handleemails} />
-                        <label htmlFor="email" className='email-label' id='email-labelled'>Email</label>
-                        <span style={{color:"red",fontWeight:"bolder"}}>{emailError}</span>
+                    <div style={{marginBottom:"0px"}}>
+                        <div className='input-content infocus' id='input-email'>
+                            <i class="fa fa-envelope"></i>
+                            <input type="email" name="email" className='email-input' autoComplete="off" id="email" required value={email} onChange={handleemails} />
+                            <label htmlFor="email" className='email-label' id='email-labelled'>Email</label>
+
+                        </div>
+                        <p style={{ color: "red", fontWeight: "bolder", marginTop: "0px" }} id='email-errp'>{emailError}</p>
                     </div>
-                    
-                    <div className='input-content infocus' id='input-password'>
-                        <i class="fa fa-lock"></i>
-                        <input type="password" name="password" className='password-input' id="password" required value={password} onChange={handlepasswords} />
-                        <label htmlFor="password" className='password-label' id='password-labelled'>Password</label>
-                        <span style={{color:"red",fontWeight:"bolder"}}>{passwordError}</span>
+                    <div style={{marginTop:"0px"}}>
+                        <div className='input-content infocus' id='input-password'>
+                            <i class="fa fa-lock"></i>
+                            <input type="password" name="password" className='password-input' id="password" required value={password} onChange={handlepasswords} />
+                            <label htmlFor="password" className='password-label' id='password-labelled'>Password</label>
+
+                        </div>
+                        <p style={{ color: "red", fontWeight: "bolder",marginTop:"0px" }} id='pass-errp'>{passwordError}</p>
                     </div>
                     <div className="forgot"> <a href="/"> Forgot my password </a></div>
                     <NavLink to="/login">
