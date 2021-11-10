6d790edbb350930df9fd94866e1497041662ef79
Atul Kumar <atulkumarrpvvyv0740@gmail.com>
2021-11-10 08:59:12 +0530

    10/11-08:59

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
-// import Form from './components/LoginForm';
-// import { Route,Switch,BrowserRouter } from 'react-router-dom';
-// import Register from './components/Register';
 import Master from './components/Master';
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter>
-        <Switch>
-          <Route path='/' exact component={Form}></Route>
-          <Route path='/signup' exact component={Register} ></Route>
-          <Route path='/login' component={Form}></Route>
-          <Route path='/loginpage' component={Form}></Route>
-        </Switch>
-      </BrowserRouter> */}
       <Master/>
     </div>
   );
